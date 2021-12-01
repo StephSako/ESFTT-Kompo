6db1a7bc6f66aa7c66e2f4c2cd03ed20cc244879
Steph Sako <stephen.sakovitch@orange.fr>
2021-12-01 18:53:46 +0100

    Fix responsive table resizing in Contacter, optimize resizing in script.js

--- public/JS/resizeResponsiveTable.js
+++ public/JS/resizeResponsiveTable.js
@@ -4,3 +4,11 @@ $(window).resize(function() {
         trs.forEach(element => element.style.height = Math.max(...trs.map(x => x.offsetHeight)) + 'px');
     } else trs.forEach(element => element.style.height = 'auto');
 });
+
+
+$(document).ready(function() {
+    if ($(window).width() <= 992) {
+        let trs = [...document.getElementById('table-to-adapt-columns').rows];
+        trs.forEach(element => element.style.height = Math.max(...trs.map(x => x.offsetHeight)) + 'px');
+    }
+});
--- public/JS/script.js
+++ public/JS/script.js
@@ -24,11 +24,4 @@ $(document).ready(function() {
     $('.collapsible').collapsible({
         accordion : false
     });
-
-    $(document).ready(function() {
-        if ($(window).width() <= 992) {
-            let trs = [...document.getElementById('table-to-adapt-columns').rows];
-            trs.forEach(element => element.style.height = Math.max(...trs.map(x => x.offsetHeight)) + 'px');
-        }
-    });
 });
