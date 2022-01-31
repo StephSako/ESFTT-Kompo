a502a2baa05a1746b9eda0aa6c120c47b23fd1ab
Steph Sako <stephen.sakovitch@orange.fr>
2022-01-31 18:42:30 +0100

    Fix bug FFTT's API new membre without licence

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -212,7 +212,6 @@ class HomeController extends AbstractController
         }));
 
         $api = new FFTTApi($this->getParameter('fftt_api_login'), $this->getParameter('fftt_api_password'));
-        $historique = array_slice($api->getHistoriqueJoueurByLicence($this->getUser()->getLicence()), -5);
         return $this->render('journee/index.html.twig', [
             'journee' => $journee,
             'idJournee' => $numJournee,
@@ -615,9 +614,9 @@ class HomeController extends AbstractController
                 return [
                     'nom' => $joueur->getNom() . ' ' . $joueur->getPrenom(),
                     'avatar' => ($joueur->getAvatar() ? 'images/profile_pictures/' . $joueur->getAvatar() : 'images/account.png'),
-                    'pointsVirtuelsPointsWonMensuel' => $virtualPoint->getVirtualPoints() != 0.0 ? $virtualPoint->getPointsWon() : null,
-                    'pointsVirtuelsVirtualPoints' => $virtualPoint->getVirtualPoints() != 0.0 ? $virtualPoint->getVirtualPoints() : null,
-                    'pointsVirtuelsPointsWonPhase' => $virtualPoint->getVirtualPoints() - $joueur->getClassementOfficiel()
+                    'pointsVirtuelsPointsWonMensuel' => $joueur->getLicence() && $virtualPoint->getVirtualPoints() != 0.0 ? $virtualPoint->getPointsWon() : null,
+                    'pointsVirtuelsVirtualPoints' => $joueur->getLicence() && $virtualPoint->getVirtualPoints() != 0.0 ? $virtualPoint->getVirtualPoints() : null,
+                    'pointsVirtuelsPointsWonPhase' => $joueur->getLicence() ? $virtualPoint->getVirtualPoints() - $joueur->getClassementOfficiel() : null
                 ];
             }, $competiteurs);
             $classementProgressionPhase = $classementProgressionMensuel;
