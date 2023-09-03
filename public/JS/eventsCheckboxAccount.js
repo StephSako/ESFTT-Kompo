b8fc77504a048c3229efae92c45d8bb1c1fe00f4
Steph Sako <stephen.sakovitch@orange.fr>
2023-09-03 17:26:35 +0200

    "Convoquer les joueurs", fix wording alert contactables dans account

--- public/JS/eventsCheckboxAccount.js
+++ public/JS/eventsCheckboxAccount.js
@@ -1,5 +1,7 @@
 $(document).ready(() => {
-    let spanSMS = $('span.parSMS');
+    let spanMail = $('.parMail');
+    let spanSMS = $('.parSMS');
+    let niMailSMS = $('.niMailSMS');
     let encadreNotContactable = $('#encadreNotContactable');
 
     let i_mail = $('#i_mail');
@@ -13,7 +15,7 @@ $(document).ready(() => {
     let cb_c_num2 = $('#cb_c_num_2');
 
     /** Affichage de l'encadré alertant de la non contactabilité **/
-    manageNotContactableEncadre(cb_c_mail, cb_c_mail2, cb_c_num, cb_c_num2, encadreNotContactable, spanSMS, false);
+    manageNotContactableEncadre(cb_c_mail, cb_c_mail2, cb_c_num, cb_c_num2, encadreNotContactable, spanMail, spanSMS, niMailSMS, false);
 
     /** Evènements liés aux emails et numéros de téléphone **/
     if (!i_mail.val()) cb_c_mail.prop('checked', false).prop('disabled', true);
@@ -24,7 +26,7 @@ $(document).ready(() => {
     i_mail.on('keyup', () => {
         if (!i_mail.val()) {
             cb_c_mail.prop('checked', false);
-            manageNotContactableEncadre(cb_c_mail, cb_c_mail2, cb_c_num, cb_c_num2, encadreNotContactable, spanSMS, true);
+            manageNotContactableEncadre(cb_c_mail, cb_c_mail2, cb_c_num, cb_c_num2, encadreNotContactable, spanMail, spanSMS, niMailSMS, true);
         }
         cb_c_mail.prop('disabled', !i_mail.val());
     });
@@ -32,7 +34,7 @@ $(document).ready(() => {
     i_mail2.on('keyup', () => {
         if (!i_mail2.val()) {
             cb_c_mail2.prop('checked', false);
-            manageNotContactableEncadre(cb_c_mail, cb_c_mail2, cb_c_num, cb_c_num2, encadreNotContactable, spanSMS, true);
+            manageNotContactableEncadre(cb_c_mail, cb_c_mail2, cb_c_num, cb_c_num2, encadreNotContactable, spanMail, spanSMS, niMailSMS, true);
         }
         cb_c_mail2.prop('disabled', !i_mail2.val());
     });
@@ -40,7 +42,7 @@ $(document).ready(() => {
     i_num.on('keyup', () => {
         if (!iphoneNumberValid(i_num.val())) {
             cb_c_num.prop('checked', false);
-            manageNotContactableEncadre(cb_c_mail, cb_c_mail2, cb_c_num, cb_c_num2, encadreNotContactable, spanSMS, true);
+            manageNotContactableEncadre(cb_c_mail, cb_c_mail2, cb_c_num, cb_c_num2, encadreNotContactable, spanMail, spanSMS, niMailSMS, true);
         }
         cb_c_num.prop('disabled', !iphoneNumberValid(i_num.val()));
     });
@@ -48,25 +50,25 @@ $(document).ready(() => {
     i_num2.on('keyup', () => {
         if (!iphoneNumberValid(i_num2.val())) {
             cb_c_num2.prop('checked', false);
-            manageNotContactableEncadre(cb_c_mail, cb_c_mail2, cb_c_num, cb_c_num2, encadreNotContactable, spanSMS, true);
+            manageNotContactableEncadre(cb_c_mail, cb_c_mail2, cb_c_num, cb_c_num2, encadreNotContactable, spanMail, spanSMS, niMailSMS, true);
         }
         cb_c_num2.prop('disabled', !iphoneNumberValid(i_num2.val()));
     });
 
     cb_c_mail.on('change', () => {
-        manageNotContactableEncadre(cb_c_mail, cb_c_mail2, cb_c_num, cb_c_num2, encadreNotContactable, spanSMS, true);
+        manageNotContactableEncadre(cb_c_mail, cb_c_mail2, cb_c_num, cb_c_num2, encadreNotContactable, spanMail, spanSMS, niMailSMS, true);
     });
 
     cb_c_mail2.on('change', () => {
-        manageNotContactableEncadre(cb_c_mail, cb_c_mail2, cb_c_num, cb_c_num2, encadreNotContactable, spanSMS, true);
+        manageNotContactableEncadre(cb_c_mail, cb_c_mail2, cb_c_num, cb_c_num2, encadreNotContactable, spanMail, spanSMS, niMailSMS, true);
     });
 
     cb_c_num.on('change', () => {
-        manageNotContactableEncadre(cb_c_mail, cb_c_mail2, cb_c_num, cb_c_num2, encadreNotContactable, spanSMS, true);
+        manageNotContactableEncadre(cb_c_mail, cb_c_mail2, cb_c_num, cb_c_num2, encadreNotContactable, spanMail, spanSMS, niMailSMS, true);
     });
 
     cb_c_num2.on('change', () => {
-        manageNotContactableEncadre(cb_c_mail, cb_c_mail2, cb_c_num, cb_c_num2, encadreNotContactable, spanSMS, true);
+        manageNotContactableEncadre(cb_c_mail, cb_c_mail2, cb_c_num, cb_c_num2, encadreNotContactable, spanMail, spanSMS, niMailSMS, true);
     });
 });
 
@@ -74,15 +76,22 @@ function iphoneNumberValid(phoneNumber) {
     return phoneNumber.length === 10 && phoneNumber.startsWith('0');
 }
 
-function manageNotContactableEncadre(cb_c_mail, cb_c_mail2, cb_c_num, cb_c_num2, encadreNotContactable, spanSMS, editing) {
+function manageNotContactableEncadre(cb_c_mail, cb_c_mail2, cb_c_num, cb_c_num2, encadreNotContactable, spanMail, spanSMS, niMailSMS, editing) {
     if (editing) {
         isNotContactableMail = !(cb_c_mail.is(":checked") || cb_c_mail2.is(":checked"));
         isNotContactableSMS = !(cb_c_num.is(":checked") || cb_c_num2.is(":checked"));
     }
 
-    if (isNotContactableMail) encadreNotContactable.show()
-    else encadreNotContactable.hide()
+    if (isNotContactableMail || isNotContactableSMS) {
+        encadreNotContactable.show()
 
-    if (isNotContactableSMS) spanSMS.show()
-    else spanSMS.hide()
+        if (isNotContactableMail) spanMail.show()
+        else spanMail.hide()
+
+        if (isNotContactableSMS) spanSMS.show()
+        else spanSMS.hide()
+
+        if (isNotContactableMail && isNotContactableSMS) niMailSMS.show()
+        else niMailSMS.hide()
+    } else encadreNotContactable.hide()
 }
